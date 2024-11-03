1ae5339e70c1af1a8398ea24f13912da75245cfb
unknown <ahmedirheem2003@gmail.com>
2024-11-03 15:39:18 +0200

    Sign up Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,20 @@ module.exports = {
     extend: {
       fontFamily: {
         cairo: ['Cairo', 'sans-serif'],
+      },
+      colors: {
+        mainGreen: '#69BE96',
+        textGreen: '#00BE64',
+        textGray: '#8989A2',
+        lightGray: '#F7F9FB',
+      },
+      backgroundImage: {
+        backgroundEffect: "url('../src/assets/images/background-effect.png')"
       }
     },
   },
-  plugins: [],
+  plugins: [
+    // require('@tailwindcss/forms'),
+  ],
 }
 
